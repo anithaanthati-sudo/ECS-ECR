0a82d97b4c66b10fb626b6074229989f5e89aa16
anithaanthati-sudo <anitha.anthati@gmail.com>
2026-06-16 13:17:59 +0530

    Initial ECS ECR demo project

---
+++ ECS-ECR/test/Terraform/ECS/main.tf
@@ -0,0 +1,8 @@
+resource "aws_ecs_cluster" "dev_cluster" {
+  name = var.cluster_name
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
---
+++ ECS-ECR/test/Terraform/ECS/variables.tf
@@ -0,0 +1,9 @@
+
+variable "cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+ 
+}
+variable "vpc_id" {
+
+}
